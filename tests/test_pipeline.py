fb16db8f4fe34ecfa168cb5cd1203351fd0ade63
sanidavidanagama <sanidavidanagama@gmail.com>
2026-05-08 16:25:33 +0530

    test: update testing to match the schema outputs

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,29 +1,29 @@
 from src.pipeline import predict
 
-def test_introvert(): 
+def test_extrovert():
     data = {
-        "Time_spent_Alone": 1.0,
-        "Stage_fear": "No",
-        "Social_event_attendance": 8.0,
-        "Going_outside": 6.0,
-        "Drained_after_socializing": "No",
-        "Friends_circle_size": 12.0,
-        "Post_frequency": 7.0
+        "time_spent_alone": 1.0,
+        "stage_fear": "No",
+        "social_event_attendance": 8.0,
+        "going_outside": 6.0,
+        "drained_after_socializing": "No",
+        "friends_circle_size": 12.0,
+        "post_frequency": 7.0
     }
     result = predict(data)
     assert result["personality"] == "Extrovert"
     print(result)
 
-def test_extrovert():
+def test_introvert():
     data = {
-        "Time_spent_Alone": 9.0,
-        "Stage_fear": "Yes",
-        "Social_event_attendance": 1.0,
-        "Going_outside": 0.0,
-        "Drained_after_socializing": "Yes",
-        "Friends_circle_size": 1.0,
-        "Post_frequency": 1.0
-    }   
+        "time_spent_alone": 9.0,
+        "stage_fear": "Yes",
+        "social_event_attendance": 1.0,
+        "going_outside": 0.0,
+        "drained_after_socializing": "Yes",
+        "friends_circle_size": 1.0,
+        "post_frequency": 1.0
+    }
     result = predict(data)
     assert result["personality"] == "Introvert"
     print(result)
